20c43485d98711e6d2da912239227515d893f48b
zq9120 <zq9120@yeah.net>
2019-02-20 15:46:14 +0800

    update

--- src/main/java/net/floodlightcontroller/forwarding/Forwarding.java
+++ src/main/java/net/floodlightcontroller/forwarding/Forwarding.java
@@ -1200,34 +1200,34 @@ public class Forwarding extends ForwardingBase implements IFloodlightModule, IOF
      * @param cntx The FloodlightContext associated with this OFPacketIn
      */
     protected void doFlood(IOFSwitch sw, OFPacketIn pi, IRoutingDecision decision, FloodlightContext cntx) {
-//        OFPort inPort = OFMessageUtils.getInPort(pi);
-//        OFPacketOut.Builder pob = sw.getOFFactory().buildPacketOut();
-//        List<OFAction> actions = new ArrayList<>();
-//        Set<OFPort> broadcastPorts = this.topologyService.getSwitchBroadcastPorts(sw.getId());
-//        
-//        if (broadcastPorts.isEmpty()) {
-//            log.debug("No broadcast ports found. Using FLOOD output action");
-//            broadcastPorts = Collections.singleton(OFPort.FLOOD);
-//        }
-//
-//        for (OFPort p : broadcastPorts) {
-//            if (p.equals(inPort)) continue;
-//            actions.add(sw.getOFFactory().actions().output(p, Integer.MAX_VALUE));
-//        }
-//        pob.setActions(actions);
-//
-//        // set buffer-id, in-port and packet-data based on packet-in
-//        pob.setBufferId(OFBufferId.NO_BUFFER);
-//        OFMessageUtils.setInPort(pob, inPort);
-//        pob.setData(pi.getData());
-//
+        OFPort inPort = OFMessageUtils.getInPort(pi);
+        OFPacketOut.Builder pob = sw.getOFFactory().buildPacketOut();
+        List<OFAction> actions = new ArrayList<>();
+        Set<OFPort> broadcastPorts = this.topologyService.getSwitchBroadcastPorts(sw.getId());
+        
+        if (broadcastPorts.isEmpty()) {
+            log.debug("No broadcast ports found. Using FLOOD output action");
+            broadcastPorts = Collections.singleton(OFPort.FLOOD);
+        }
+
+        for (OFPort p : broadcastPorts) {
+            if (p.equals(inPort)) continue;
+            actions.add(sw.getOFFactory().actions().output(p, Integer.MAX_VALUE));
+        }
+        pob.setActions(actions);
+
+        // set buffer-id, in-port and packet-data based on packet-in
+        pob.setBufferId(OFBufferId.NO_BUFFER);
+        OFMessageUtils.setInPort(pob, inPort);
+        pob.setData(pi.getData());
+
 //        if (log.isTraceEnabled()) {
-//            log.trace("Writing flood PacketOut switch={} packet-in={} packet-out={}",
-//                    new Object[] {sw, pi, pob.build()});
+            log.info("Writing flood PacketOut switch={} packet-in={} packet-out={}",
+                    new Object[] {sw, pi, pob.build()});
 //        }
-//        messageDamper.write(sw, pob.build());
-//
-//        return;
+        messageDamper.write(sw, pob.build());
+
+        return;
     }
 
     /**
